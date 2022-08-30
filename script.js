eacccb60a323c9cd79510f5b843df0b1f8262b56
robetto <robertobettoni@gmail.com>
2022-08-30 16:41:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,98 @@
+const canvas = document.getElementById("canvas1");
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = 900;
+canvas.height = 600;
+
+// global variables
+const cellSize = 100;
+const cellGap = 3;
+const gameGrid = [];
+
+// mouse
+const mouse = {
+  x: 11,
+  y: 11,
+  width: 0.1,
+  height: 0.1,
+};
+
+let canvasPosition = canvas.getBoundingClientRect();
+canvas.addEventListener("mousemove", function (e) {
+  mouse.x = e.x - canvasPosition.left;
+  mouse.y = e.y - canvasPosition.top;
+});
+canvas.addEventListener("mouseleave", function () {
+  mouse.x = undefined;
+  mouse.y = undefined;
+});
+
+//game board
+const controlsBar = {
+  width: canvas.width,
+  height: cellSize,
+};
+
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+  }
+  draw() {
+    if (collision(this, mouse)) {
+      ctx.strokeStyle = "black";
+      ctx.strokeRect(this.x, this.y, this.width, this.height);
+    }
+  }
+}
+
+function createGrid() {
+  for (let y = cellSize; y < canvas.height; y += cellSize) {
+    // righe (saltando la prima barra di controllo)
+    for (let x = 0; x < canvas.width; x += cellSize) {
+      // loop delle colonne
+      gameGrid.push(new Cell(x, y));
+    }
+  }
+}
+
+createGrid();
+
+function handleGameGrid() {
+  for (let i = 0; i < gameGrid.length; i++) {
+    gameGrid[i].draw();
+  }
+}
+
+// projectiles
+
+//defenders
+
+//enemies
+
+// resources
+
+// utilities
+function animate() {
+  ctx.fillStyle = "blue";
+  ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
+  handleGameGrid();
+  requestAnimationFrame(animate);
+}
+animate();
+
+function collision(first, second) {
+  if (
+    !(
+      first.x > second.x + second.width ||
+      first.x + first.width < second.x ||
+      first.y > second.y + second.height ||
+      first.y + first.height < second.y
+    )
+  ) {
+    return true;
+  }
+}
